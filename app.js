6e34f5667f0a206876def9c34e0a01d22966da0b
Nisarg Patel <nisargpatel460@gmail.com>
2025-11-14 22:36:32 +0530

    add atlas_Url

--- app.js
+++ app.js
@@ -28,16 +28,19 @@ app.use(methodOverride("_method"));
 app.engine("ejs", ejsMate);
 app.use(express.static(path.join(__dirname, "/public")));
 
+const dbUrl = process.env.ATLAS_URL;
+
 main()
     .then(() => console.log("connected to database"))
     .catch((err) => console.log(err));
 
 async function main() {
-    await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+    // await mongoose.connect("mongodb://127.0.0.1:27017/wanderlust");
+    await mongoose.connect(dbUrl);
 }
 
 const sessionOption = {
-    secret: "mysupersecretcode",
+    secret: process.env.SECRET,
     resave: false,
     saveUninitialized: true,
     cookie: {
