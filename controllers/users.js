8f75d9521fcdcc6196b60bcc046256c6d40a666a
Nisarg Patel <nisargpatel460@gmail.com>
2025-12-03 22:17:33 +0530

    correct some bugs

--- controllers/users.js
+++ controllers/users.js
@@ -14,7 +14,7 @@ module.exports.signup = async(req, res) => {
         if(err) {
             return next(err);
         }
-        req.flash("success", "Welcome to Wanderlust");
+        req.flash("success", "Welcome to Stayspot");
         res.redirect("/listing");
     })
     //console.log(registerUser);
@@ -30,7 +30,7 @@ module.exports.renderLoginForm = (req, res) => {
 }
 
 module.exports.login = async(req, res) => {
-    req.flash("success", "Welcome back Wanderlust!");
+    req.flash("success", "Welcome back Stayspot!");
     let redirectUrl = res.locals.redirectUrl || "/listing";
     res.redirect(redirectUrl);
 }
